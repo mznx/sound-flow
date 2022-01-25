5b4e4065e0eb404e5c48ae71739991f0df59e59e
mznx <mznx.ux@gmail.com>
2022-01-25 18:00:22 +0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import "assets/styles/main.scss";
+
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class Home extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.home {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+</style>
