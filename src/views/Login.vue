9edec40ef8ca91cd4f97803093b99044a6b71d32
mznx <mznx.ux@gmail.com>
2022-01-26 10:54:35 +0400

    add Login view

---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login">
+    <h1>Пожалуйста, авторизуйтесь через Spotify.</h1>
+    <button @click="login" class="btn">Войти</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {
+    login() {
+      // login process
+    },
+  },
+  mounted() {
+    // check token
+  },
+})
+export default class Home extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.login {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+</style>
