d1e2596e61d12845a2743dc4d6483f745a614d9c
mznx <mznx.ux@gmail.com>
2022-01-29 10:34:40 +0400

    add vue-inline-svg dependencie

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import InlineSvg from "vue-inline-svg";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).component("inline-svg", InlineSvg).mount("#app");
