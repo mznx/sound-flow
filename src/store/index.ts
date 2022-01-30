d3ea40832c3cabf6be7226dd4c7d35501d083f38
mznx <mznx.ux@gmail.com>
2022-01-30 12:40:00 +0400

    update vuex-store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,33 +1,12 @@
 import { createStore } from "vuex";
 
+import player from "./modules/player";
+
 export default createStore({
-  state: {
-    status: "pause",
-    player: null,
-    device_id: null,
-    shuffle_state: false,
-    repeat_mode: "off",
-  },
-
-  mutations: {
-    setStatus(state, status) {
-      state.status = status;
-    },
-
-    setPlayer(state, player) {
-      state.player = player;
-    },
-
-    setDeviceID(state, device_id) {
-      state.device_id = device_id;
-    },
-
-    setShuffleState(state, shuffle_state) {
-      state.shuffle_state = shuffle_state;
-    },
-
-    setRepeatMode(state, repeat_mode) {
-      state.repeat_mode = repeat_mode;
-    },
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    player,
   },
 });
