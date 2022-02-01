63d9c4926f15e2b988bb2e53f60b6122bc2dc5bb
mznx <mznx.ux@gmail.com>
2022-02-01 15:32:47 +0400

    update api/spotify/search

--- src/api/spotify/search/index.ts
+++ src/api/spotify/search/index.ts
@@ -1,20 +1,15 @@
 import * as API from "@/types/API";
+import * as utils from "@/utils";
 import request from "../request";
 
 export default {
   search(
     opts: SpotifyApi.SearchForItemParameterObject
   ): Promise<SpotifyApi.SearchResponse | API.NullOrError> {
+    const query = utils.paramObjToQueryStr(opts);
     const req_options: API.Request = {
       method: "GET",
-      path: `/search?\
-        q=${opts.q}\
-        &type=${opts.type}\
-        $include_external=${opts.include_external}\
-        $limit=${opts.limit}\
-        &offset=${opts.offset}\
-        &market=${opts.market}\
-      `,
+      path: `/search?${query}`,
     };
     return request<SpotifyApi.SearchResponse>(req_options);
   },
