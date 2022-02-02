fa635fda2e295f7a3856f0e67beaf252027bc65d
mznx <mznx.ux@gmail.com>
2022-02-02 21:06:28 +0400

    add AppLayout layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <router-view />
+  <AppLayout />
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import AppLayout from "@/layouts/AppLayout.vue";
+
+@Options({
+  components: {
+    AppLayout,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 @import "assets/styles/main.scss";
---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class AppLayout extends Vue {}
+</script>
+
+<style scoped lang="scss"></style>
