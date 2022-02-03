0ab57b8f8fb1664400260b7e15f6c7f40d719699
mznx <mznx.ux@gmail.com>
2022-02-03 15:47:29 +0400

    delete vue-slider-component dependencie

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import InlineSvg from "vue-inline-svg";
-import VueSlider from "vue-slider-component";
-import "vue-slider-component/theme/default.css";
 import store from "@/store";
 
 createApp(App)
   .use(router)
   .use(store)
   .component("inline-svg", InlineSvg)
-  .component("VueSlider", VueSlider)
   .mount("#app");
