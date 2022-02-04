e512dbb4cef6427e31e2ef2787f88d50e945f930
mznx <mznx.ux@gmail.com>
2022-02-05 00:18:51 +0400

    update Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,25 @@
 <template>
   <CircleLoader v-if="status !== 'loaded'" />
   <div class="home" v-else>
-    <p>a1</p>
-    <router-view />
-    <p>a2</p>
-    <!-- <ControlBar /> -->
+    <InfoBar />
+    <div class="home-content">
+      <router-view />
+    </div>
+    <ControlBar />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 import CircleLoader from "@/components/CircleLoader/index.vue";
+import InfoBar from "@/components/InfoBar/index.vue";
 import ControlBar from "@/components/ControlBar/index.vue";
 
 @Options({
   components: {
     CircleLoader,
+    InfoBar,
     ControlBar,
   },
 
@@ -26,7 +29,11 @@ import ControlBar from "@/components/ControlBar/index.vue";
     }),
   },
 
-  methods: {},
+  methods: {
+    ...mapActions({
+      init: "player/init",
+    }),
+  },
 
   created() {
     /* --- */ console.log("[debug] Home (created)");
@@ -36,6 +43,11 @@ import ControlBar from "@/components/ControlBar/index.vue";
     }
 
     // player.init
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+    document.body.appendChild(script);
+    this.init();
   },
 })
 export default class Home extends Vue {}
@@ -46,5 +58,12 @@ export default class Home extends Vue {}
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.home-content {
+  width: 100%;
+  height: 100%;
 }
 </style>
