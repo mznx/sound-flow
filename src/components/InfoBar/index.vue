85dde4c5e3648dea12b5e304138d80802984f886
mznx <mznx.ux@gmail.com>
2022-02-05 00:18:00 +0400

    add InfoBar component

---
+++ src/components/InfoBar/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="info-bar"></div>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from "vue-class-component";
+
+@Options({})
+export default class ControlBar extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.info-bar {
+  width: 100%;
+  height: 50px;
+  background-color: var(--color-fg);
+  border-bottom: 1px solid #594d7e;
+  flex-shrink: 0;
+}
+</style>
