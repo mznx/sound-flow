c8f3ab7dbd92e0e24b61471c29eedb3f1c8be835
mznx <mznx.ux@gmail.com>
2022-02-07 19:53:25 +0400

    update SpotifyApiExtension types

--- src/types/SpotifyApiExtension/index.d.ts
+++ src/types/SpotifyApiExtension/index.d.ts
@@ -1,303 +1,489 @@
 declare namespace SpotifyApi {
   // albums
   interface GetAlbumParameterObject {
-    id: string;
-    market?: string;
+    params: {
+      id: string;
+    };
+    query?: {
+      market?: string;
+    };
   }
 
   interface GetAlbumsParameterObject {
-    ids: string;
-    market?: string;
+    query: {
+      ids: string;
+      market?: string;
+    };
   }
 
   interface GetAlbumsTracksParameterObject {
-    id: string;
-    limit?: number;
-    offset?: number;
-    market?: string;
+    params: {
+      id: string;
+    };
+    query?: {
+      limit?: number;
+      offset?: number;
+      market?: string;
+    };
   }
 
   interface GetSavedAlbumsParameterObject {
-    limit?: number;
-    offset?: number;
-    market?: string;
+    query?: {
+      limit?: number;
+      offset?: number;
+      market?: string;
+    };
   }
 
   interface SaveAlbumsParameterObject {
-    ids: string[];
+    body: {
+      ids: string[];
+    };
   }
 
   interface RemoveAlbumsParameterObject {
-    ids: string[];
+    body: {
+      ids: string[];
+    };
   }
 
   interface CheckSavedAlbumsParameterObject {
-    ids: string;
+    query: {
+      ids: string;
+    };
   }
 
   interface getNewReleasesParameterObject {
-    limit?: number;
-    offset?: number;
-    country?: string;
+    query?: {
+      country?: string;
+      limit?: number;
+      offset?: number;
+    };
   }
 
   // artists
   interface GetArtistParameterObject {
-    id: string;
+    params: {
+      id: string;
+    };
   }
 
   interface GetArtistsParameterObject {
-    ids: string;
+    query: {
+      ids: string;
+    };
   }
 
   interface GetArtistAlbumsParameterObject {
-    id: string;
-    include_groups?: string;
-    limit?: number;
-    offset?: number;
-    market?: string;
+    params: {
+      id: string;
+    };
+    query?: {
+      include_groups?: string;
+      limit?: number;
+      market?: string;
+      offset?: number;
+    };
   }
 
-  interface GetArtisTopTracksParameterObject {
-    id: string;
-    market?: string;
+  interface GetArtistTopTracksParameterObject {
+    params: {
+      id: string;
+    };
+    query?: {
+      market?: string;
+    };
   }
 
   interface GetArtistRelatedArtistsParameterObject {
-    id: string;
+    params: {
+      id: string;
+    };
   }
 
   // tracks
   interface GetTrackParameterObject {
-    id: string;
-    market?: string;
+    params: {
+      id: string;
+    };
+    query?: {
+      market?: string;
+    };
   }
 
   interface GetTracksParameterObject {
-    ids: string;
-    market?: string;
+    query: {
+      ids: string;
+      market?: string;
+    };
   }
 
   interface GetSavedTracksParameterObject {
-    limit?: number;
-    offset?: number;
-    market?: string;
+    query?: {
+      limit?: number;
+      market?: string;
+      offset?: number;
+    };
   }
 
   interface SaveTracksParameterObject {
-    ids: string[];
+    body: {
+      ids: string[];
+    };
   }
 
   interface RemoveTracksParameterObject {
-    ids: string[];
+    body: {
+      ids: string[];
+    };
   }
 
   interface CheckSavedTracksParameterObject {
-    ids: string;
+    query: {
+      ids: string[];
+    };
   }
 
   interface GetTracksAudioFeaturesParameterObject {
-    ids: string;
+    query: {
+      ids: string[];
+    };
   }
 
   interface GetTrackAudioFeaturesParameterObject {
-    id: string;
+    params: {
+      id: string;
+    };
   }
 
   interface GetTrackAudioAnalysisParameterObject {
-    id: string;
+    params: {
+      id: string;
+    };
+  }
+
+  interface GetRecommendationsParameterObject {
+    query: RecommendationsOptionsObject;
+  }
+
+  // search
+  interface SearchParameterObject {
+    query: SearchForItemParameterObject;
   }
 
   // users
   interface GetUserTopArtistsParameterObject {
-    limit?: number;
-    offset?: number;
-    time_range?: string;
+    query?: {
+      limit?: number;
+      offset?: number;
+      time_range?: string;
+    };
   }
 
   interface GetUserTopTracksParameterObject {
-    limit?: number;
-    offset?: number;
-    time_range?: string;
+    query?: {
+      limit?: number;
+      offset?: number;
+      time_range?: string;
+    };
   }
 
   interface GetUserProfileParameterObject {
-    user_id: string;
+    params: {
+      user_id: string;
+    };
   }
 
   interface FollowPlaylistParameterObject {
-    playlist_id: string;
-    public?: boolean;
+    params: {
+      playlist_id: string;
+    };
+    body?: {
+      public?: boolean;
+    };
   }
 
   interface UnfollowPlaylistParameterObject {
-    playlist_id: string;
+    params: {
+      playlist_id: string;
+    };
   }
 
   interface GetFollowedArtistsParameterObject {
-    type: string;
-    limit?: number;
-    after?: string;
+    query: {
+      type: string;
+      limit?: number;
+      after?: string;
+    };
   }
 
   interface FollowArtistsOrUsersParameterObject {
-    ids: string;
-    type: string;
+    query: {
+      type: string;
+    };
+    body: {
+      ids: string[];
+    };
   }
 
   interface UnfollowArtistsOrUsersParameterObject {
-    ids: string;
-    type: string;
+    query: {
+      type: string;
+    };
+    body: {
+      ids: string[];
+    };
   }
 
   interface CheckUserFollowArtistsOrUsersParameterObject {
-    ids: string;
-    type: string;
+    query: {
+      ids: string;
+      type: string;
+    };
   }
 
   interface CheckUsersFollowPlaylistParameterObject {
-    playlist_id: string;
-    ids: string;
+    params: {
+      playlist_id: string;
+    };
+    query: {
+      ids: string;
+    };
   }
 
   // playlists
   interface GetPlaylistParameterObject {
-    playlist_id: string;
-    fields?: string;
-    market?: string;
+    params: {
+      playlist_id: string;
+    };
+    query?: {
+      fields?: string;
+      market?: string;
+    };
   }
 
   interface ChangePlaylistDetailsParameterObject {
-    playlist_id: string;
-    name?: string;
-    public?: boolean;
-    collaborative?: boolean;
-    description?: string;
+    params: {
+      playlist_id: string;
+    };
+    body?: {
+      name?: string;
+      public?: boolean;
+      collaborative?: boolean;
+      description?: string;
+    };
   }
 
   interface GetPlaylistTracksParameterObject {
-    playlist_id: string;
-    fields?: string;
-    limit?: number;
-    offset?: number;
-    market?: string;
+    params: {
+      playlist_id: string;
+    };
+    query?: {
+      fields?: string;
+      limit?: number;
+      market?: string;
+      offset?: number;
+    };
   }
 
   interface AddTracksToPlaylistParameterObject {
-    playlist_id: string;
-    position?: number;
-    uris?: string[];
+    params: {
+      playlist_id: string;
+    };
+    body?: {
+      uris?: string[];
+      position?: number;
+    };
   }
 
   interface ReorderPlaylistTracksParameterObject {
-    playlist_id: string;
-    range_start?: number;
-    insert_before?: number;
-    range_length?: number;
-    snapshot_id?: string;
+    params: {
+      playlist_id: string;
+    };
+    body?: {
+      range_start?: number;
+      insert_before?: number;
+      range_length?: number;
+      snapshot_id?: string;
+    };
   }
 
   interface ReplacePlaylistTracksParameterObject {
-    playlist_id: string;
-    uris?: string[];
+    params: {
+      playlist_id: string;
+    };
+    body?: {
+      uris?: string[];
+    };
+  }
+
+  interface SpotifyUri {
+    uri: string;
   }
 
   interface RemovePlaylistTracksParameterObject {
-    playlist_id: string;
-    tracks: SpotifyApi.TrackLinkObject[];
-    snapshot_id?: string;
+    params: {
+      playlist_id: string;
+    };
+    body: {
+      tracks: SpotifyUri[];
+      snapshot_id?: string;
+    };
   }
 
   interface GetCurrentUserPlaylistsParameterObject {
-    limit?: number;
-    offset?: number;
+    query?: {
+      limit?: number;
+      offset?: number;
+    };
   }
 
-  interface GetUserPlaylistsParameterObject
-    extends GetCurrentUserPlaylistsParameterObject {
-    user_id: string;
+  interface GetUserPlaylistsParameterObject {
+    params: {
+      user_id: string;
+    };
+    query?: {
+      limit?: number;
+      offset?: number;
+    };
   }
 
   interface CreatePlaylistParameterObject {
-    user_id: string;
-    name: string;
-    public?: boolean;
-    collaborative?: boolean;
-    description?: string;
+    params: {
+      user_id: string;
+    };
+    body: {
+      name: string;
+      public?: boolean;
+      collaborative?: boolean;
+      description?: string;
+    };
   }
 
   interface GetFeaturedPlaylistsParameterObject {
-    country?: string;
-    locale?: string;
-    timestamp?: string;
-    limit?: number;
-    offset?: number;
+    query?: {
+      country?: string;
+      limit?: number;
+      locale?: string;
+      offset?: number;
+      timestamp?: string;
+    };
   }
 
-  interface GetCategorysPlaylistsParameterObject {
-    category_id: string;
-    country?: string;
-    limit?: number;
-    offset?: number;
+  interface GetCategoryPlaylistsParameterObject {
+    params: {
+      category_id: string;
+    };
+    query?: {
+      country?: string;
+      limit?: number;
+      offset?: number;
+    };
   }
 
   interface GetPlaylistImageParameterObject {
-    playlist_id: string;
+    params: {
+      playlist_id: string;
+    };
   }
 
   interface SetPlaylistImageParameterObject {
-    playlist_id: string;
+    params: {
+      playlist_id: string;
+    };
   }
 
   // player
   interface GetPlaybackStateParameterObject {
-    market?: string;
+    query?: {
+      market?: string;
+    };
   }
 
   interface TransferPlaybackParameterObject {
-    device_ids: string[];
-    play?: boolean;
+    body: {
+      device_ids: string[];
+      play?: boolean;
+    };
   }
 
   interface GetCurrentTrackParameterObject {
-    market?: string;
+    query?: {
+      market?: string;
+    };
+  }
+
+  interface StartPlaybackParameterObject {
+    query?: {
+      device_id?: string;
+    };
+    body?: {
+      context_uri?: string;
+      uris?: string[];
+      offset?: { position: number };
+      position_ms?: number;
+    };
   }
 
   interface PausePlaybackParameterObject {
-    device_id?: string;
+    query?: {
+      device_id?: string;
+    };
   }
 
   interface NextTrackParameterObject {
-    device_id?: string;
+    query?: {
+      device_id?: string;
+    };
   }
 
   interface PrevTrackParameterObject {
-    device_id?: string;
+    query?: {
+      device_id?: string;
+    };
   }
 
   interface SeekToPositionParameterObject {
-    position_ms: number;
-    device_id?: string;
+    query: {
+      position_ms: number;
+      device_id?: string;
+    };
   }
 
   interface SetRepeatModeParameterObject {
-    state: string;
-    device_id?: string;
+    query: {
+      state: string;
+      device_id?: string;
+    };
   }
 
   interface SetVolumeParameterObject {
-    volume_percent: number;
-    device_id?: strin;
+    query: {
+      volume_percent: number;
+      device_id?: string;
+    };
   }
 
   interface TogglePlaybackShuffleParameterObject {
-    state: boolean;
-    device_id?: string;
+    query: {
+      state: boolean;
+      device_id?: string;
+    };
+  }
+
+  interface GetRecentlyPlayedTracksParameterObject {
+    query?: {
+      after?: number;
+      before?: number;
+      limit?: number;
+    };
   }
 
   interface AddToPlaybackQueueParameterObject {
-    uri: string;
-    device_id?: string;
+    query: {
+      uri: string;
+      device_id?: string;
+    };
   }
 
   // -----
