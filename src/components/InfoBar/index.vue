3d4c14718e3ee767bf6423cac9987e25449d14f9
mznx <mznx.ux@gmail.com>
2022-02-10 21:41:18 +0400

    update InfoBar component

--- src/components/InfoBar/index.vue
+++ src/components/InfoBar/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="info-bar"></div>
+  <div class="info-bar">
+    <div class="info-bar-control">
+      <button @click="$router.push('/')">
+        <inline-svg :src="require('@/assets/icons/info-bar-home.svg')" />
+      </button>
+      <button @click="$router.go(-1)">
+        <inline-svg :src="require('@/assets/icons/info-bar-back.svg')" />
+      </button>
+      <button @click="$router.go(1)">
+        <inline-svg :src="require('@/assets/icons/info-bar-forward.svg')" />
+      </button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,9 +24,28 @@ export default class ControlBar extends Vue {}
 <style scoped lang="scss">
 .info-bar {
   width: 100%;
-  height: 30px;
+  height: 38px;
   background-color: var(--color-fg);
   border-bottom: 1px solid var(--color-border);
   flex-shrink: 0;
+  display: flex;
+  flex-direction: row;
+}
+
+.info-bar-control {
+  button {
+    background-color: unset;
+    border: unset;
+    fill: var(--color-control);
+    height: 100%;
+    width: 40px;
+    margin: 0 5px;
+    transition: all 0.3s ease;
+
+    &:hover {
+      cursor: pointer;
+      fill: var(--color-control-light);
+    }
+  }
 }
 </style>
