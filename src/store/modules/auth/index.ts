3dcbd10cf7343d9bba72b0b99c889835bdb3238d
mznx <mznx.ux@gmail.com>
2022-02-15 17:54:16 +0400

    update vuex-store auth

--- src/store/modules/auth/index.ts
+++ src/store/modules/auth/index.ts
@@ -43,15 +43,28 @@ async function init({
 }: ActionContext<AuthState, RootState>): Promise<void> {
   /* --- */ console.log("[debug] auth/init (start)");
   dispatch("app/setStatus", "not-logged", { root: true });
-  const access_token = localStorage.getItem("access_token");
-  if (access_token) {
+  const access_token: string | null = localStorage.getItem("access_token");
+  const refresh_token: string | null = localStorage.getItem("refresh_token");
+
+  if (access_token && refresh_token) {
     /* --- */ console.log("[debug] auth/init (check start)");
-    const res: API.CheckToken = await api.backend.auth.checkToken(access_token);
-    /* --- */ console.log("[debug] auth/init (check end)");
-    if (res.status === "ok") {
+    const res_check: API.CheckToken = await api.backend.auth.checkToken(
+      access_token
+    );
+    if (res_check.status === "ok") {
       dispatch("setAccessToken", access_token);
       dispatch("app/setStatus", "logged", { root: true });
+    } else {
+      const res_refresh: API.RefreshToken = await api.backend.auth.refreshToken(
+        refresh_token
+      );
+      if ("access_token" in res_refresh) {
+        localStorage.setItem("access_token", res_refresh.access_token);
+        dispatch("setAccessToken", res_refresh.access_token);
+        dispatch("app/setStatus", "logged", { root: true });
+      }
     }
+    /* --- */ console.log("[debug] auth/init (check end)");
   }
   /* --- */ console.log("[debug] auth/init (end)");
 }
