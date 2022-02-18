5ff613ca38229a38acaa89e2fe01373be6e40bbd
mznx <mznx.ux@gmail.com>
2022-02-18 23:02:02 +0400

    add ItemCard component

---
+++ src/components/ItemCard/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="item-card" @click="openItemContext">
+    <p>{{ item.name }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from "vue-class-component";
+import * as utils from "@/utils";
+
+@Options({
+  props: {
+    item: {},
+  },
+
+  methods: {
+    openItemContext() {
+      const uri = utils.spotifyUriParse(this.item.uri);
+      this.$router.push(`/${uri.type}/${uri.val}`);
+    },
+  },
+})
+export default class ItemCard extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.item-card {
+  width: 250px;
+  height: 250px;
+  background-color: var(--color-fg);
+  border-radius: 10px;
+  color: var(--color-text);
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>
